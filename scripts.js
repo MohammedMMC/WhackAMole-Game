c7a0d2a329064f37fc3aa6239ab42951b7344afd
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-08-30 18:16:26 +0300

    3. commit

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ const pointsElement = document.querySelector("h2 span");
 
 let points = 0;
 let activeButtonIndex = -1;
+let gameDuration = 30000;
+let moleActiveTime = 800;
 let gameInterval;
+let gameTimeout;
+let gameStarted = false;
 
 function resetButtons() {
     buttons.forEach(button => {
@@ -18,6 +22,7 @@ function startMole() {
         buttons[activeButtonIndex].style.backgroundImage = "none";
         buttons[activeButtonIndex].style.backgroundColor = "grey";
         buttons[activeButtonIndex].disabled = true;
+        activeButtonIndex = -1;
     }
 
     activeButtonIndex = Math.floor(Math.random() * buttons.length);
@@ -31,11 +36,64 @@ function startMole() {
             buttons[activeButtonIndex].disabled = true;
             activeButtonIndex = -1;
         }
-    }, 800);
+    }, moleActiveTime);
+}
+
+function clickedButton(button) {
+    const clickedIndex = [...buttons].indexOf(button);
+
+    if (clickedIndex === activeButtonIndex) {
+        points++;
+        pointsElement.textContent = points;
+
+        buttons[activeButtonIndex].style.backgroundImage = "none";
+        buttons[activeButtonIndex].style.backgroundColor = "grey";
+        buttons[activeButtonIndex].disabled = true;
+        activeButtonIndex = -1;
+    }
 }
 
 function startGame() {
-    resetButtons();
-    gameInterval = setIntarvel(startMole, moleActiveTime + );
-    
+    if (!gameStarted) {
+        gameStarted = true;
+        points = 0;
+        pointsElement.textContent = points;
+        resetButtons();
+
+        gameInterval = setInterval(startMole, moleActiveTime + 200);
+
+        gameTimeout = setTimeout(() => {
+            clearInterval(gameInterval);
+            alert(`Game Over! Your final score is ${points}`);
+            resetGame();
+        }, gameDuration);
+    }
 }
+
+function resetGame() {
+    resetButtons();
+    clearInterval(gameInterval);
+    clearTimeout(gameTimeout);
+    points = 0;
+    pointsElement.textContent = points;
+    gameStarted = false;
+    buttons[4].style.backgroundImage = "none";
+    buttons[4].style.backgroundColor = "lime";
+    buttons[4].innerText = "START!";
+    buttons[4].disabled = false;
+}
+
+window.onload = () => {
+    resetGame();
+    buttons.forEach(button => {
+        button.onclick = () => { clickedButton(button); };
+    });
+
+    buttons[4].onclick = () => {
+        if (!gameStarted) {
+            buttons[4].disabled = true;
+            buttons[4].innerText = "";
+            startGame();
+        }
+    };
+};
